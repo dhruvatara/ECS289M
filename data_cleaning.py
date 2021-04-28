005f6ff87e31b47bdb46958f8634b07476148a98
dhruvatara <dhruvatara.b@gmail.com>
2021-04-28 14:18:03 -0700

    data normalized and matched up

--- data_cleaning.py
+++ data_cleaning.py
@@ -13,7 +13,7 @@ def clean_data():
 	# print(file.head())
 	root = sys.argv[1].split("\\")
 	root = root[1]+"\\"+root[2]
-	print(root)
+	# print(root)
 	#Drop azimuth column
 	cleanFile = file.drop(['gyro(x)'],axis=1)
 	# print(cleanFile.head())
@@ -21,9 +21,48 @@ def clean_data():
 	# print(cleanFile['gyro(y)'][:5])
 	normalize(cleanFile,'gyro(y)')
 	normalize(cleanFile,'gyro(z)')
+	matchup(root,cleanFile)
 	cleanFile.to_csv(root+"\\cleanedGyroReadings.csv")
 	# print(cleanFile['gyro(y)'][:5])
 	# print(yMean)
+
+def matchup(root,cleanGyro):
+	buttonPress = pd.read_csv(root+"\\button-readings.csv")
+	gyroTime = cleanGyro['timestamp']
+	buttonStart = buttonPress['pressStart']
+	buttonStop = buttonPress['pressStop']
+	
+	j = 0
+	k = 0
+	meanGyroY = 0.0
+	meanGyroZ = 0.0
+	n = 0
+	# print(gyroTime[9])
+	# print(buttonStart[1])
+	# print(buttonStop[1])
+	while (k<len(gyroTime) and j<len(buttonStop)):
+		# print("here")
+		if (gyroTime[k] >= buttonStart[j] and gyroTime[k]<=buttonStop[j]):
+			# print("here")
+			meanGyroY += cleanGyro.iloc[k]['gyro(y)']
+			meanGyroZ += cleanGyro.iloc[k]['gyro(z)']
+			n+=1
+		elif(gyroTime[k]>buttonStop[j] and n>0):
+
+			meanGyroY /= n
+			meanGyroZ /= n
+			# print(meanGyroY)
+			buttonPress.at[j,'mean gyro(y)'] = meanGyroY
+			buttonPress.at[j,'mean gyro(z)'] = meanGyroZ
+			n = 0
+			j+=1
+			k-=1
+		elif(gyroTime[k]>buttonStart[j]):
+			j+=1
+			k-=1
+		k+=1
+	buttonPress.to_csv(root+"\\combinedReadings.csv")
+
 
 def normalize(data,column):
 	#Normalize gyroscope readings
